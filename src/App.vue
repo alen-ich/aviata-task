50ea2b84b850cfe6d2b61c8d11260980e2537a05
Alen Ichshanov <a.ichshanov@adamar.solutions>
2022-12-21 22:55:59 +0600

    feat: tooltip filters

--- src/App.vue
+++ src/App.vue
@@ -88,19 +88,3 @@ const filterByAirline = (flights: Array<any>, airline: string) => {
     <TicketList :tickets="displayFlights" :airlines="dataAirlines" />
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
