1e28aa5a353a96f6f274a67531080f3f6497b1c6
Alen Ichshanov <alen.ichshanov@gmail.com>
2022-12-21 11:35:39 +0600

    fix: ticket price style

--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -46,16 +46,16 @@ const findAirline = (code: string) => {
             </div>
         </div>
         <div class="flex flex-col items-center pt-3 px-5 pb-[15px] bg-beige rounded-r">
-            <div class="text-2xl leading-7 text-deep-dark mb-3">{{ ticketData.price }} ₸</div>
+            <div class="text-2xl leading-7 text-deep-dark mb-3">{{ ticketData.price }} <span class="text-lg">₸</span></div>
             <button type="button"
                 class="rounded bg-bright-green w-[200px] h-10 text-white font-bold text-lg leading-[25px] flex justify-center items-center mb-2">Выбрать</button>
             <div class="text-dark flex justify-center items-center text-xs mb-3">
                 Цена за всех пассажиров
             </div>
             <div class="flex w-full items-center gap-x-3 justify-center">
-                <div v-if="ticketData.services.hasOwnProperty('0PC')" class="text-deep-dark text-xs">Нет багажа</div>
-                <div v-else-if="ticketData.services.hasOwnProperty('20KG')"  class="text-deep-dark text-xs">{{ ticketData.services['20KG'].alt_text }}</div>
-                <div v-else-if="ticketData.services.hasOwnProperty('1PC')"  class="text-deep-dark text-xs">{{ ticketData.services['1PC'].alt_text }}</div>
+                <div v-if="ticketData.services.hasOwnProperty('0PC')" class="text-deep-dark text-xs w-[66px] text-center">Нет багажа</div>
+                <div v-else-if="ticketData.services.hasOwnProperty('20KG')"  class="text-deep-dark text-xs w-[66px] text-center">{{ ticketData.services['20KG'].alt_text }}</div>
+                <div v-else-if="ticketData.services.hasOwnProperty('1PC')"  class="text-deep-dark text-xs w-[66px] text-center">{{ ticketData.services['1PC'].alt_text }}</div>
                 <div
                     class="flex justify-center items-center rounded-sm bg-light-blue text-deep-blue font-semibold text-xs px-2 py-[3px]">
                     + Добавить багаж</div>
